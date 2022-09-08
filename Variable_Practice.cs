c54e4796f7b94faf2dd21b279e6a7b6a3f0c9bf2
HunterBusby <97651304+HunterBusby@users.noreply.github.com>
2022-09-07 19:46:16 -0600

    Added Variable_Practice

---
+++ Variable_Practice.cs
@@ -0,0 +1,23 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		string name = "John";
+		int myNum = 15;
+		myNum = 20;
+		double myDoubleNum = 5.99D;  
+char myLetter = 'D';         
+bool myBool = true;         
+string myText = "My text example"; 
+		Console.WriteLine(name);
+		Console.WriteLine(myNum);
+		Console.WriteLine(myDoubleNum);
+		Console.WriteLine(myLetter);
+		Console.WriteLine(myBool);
+		Console.WriteLine(myText);
+
+	}
+}
