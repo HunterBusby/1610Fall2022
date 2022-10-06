08dec7a0b0a8019208de22b3ce5936637c92bbd4
Hunter Busby <10984558@uvu.edu>
2022-10-05 18:36:15 -0600

    Added more Unit 3 Scripts

--- Fall2022/Assets/BasicSyntax.cs
+++ Fall2022/Assets/BasicSyntax.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BasicSyntax : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
-        
-    }
+        //this line is there to tell me the x position of my object
 
-    // Update is called once per frame
-    void Update()
-    {
+        /*Hi there!
+         * this is two lines!
+         * */
+        Debug.Log(transform.position.x);
         
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
     }
 }
