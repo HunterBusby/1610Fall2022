0b56f3ae4bc4bf30b673da6865fb26d15b3e3794
HunterBusby <97651304+HunterBusby@users.noreply.github.com>
2022-10-21 16:17:57 -0600

    Duplicated scripts and assets from unit 5 for ease of access. somethng else happened. a lot of files Just showed up

---
+++ Fall2022Firstgame/Assets/ObjectColorChangeRGB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectColorChangeRGB : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer> ().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer> ().material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer> ().material.color = Color.blue;
+        }
+    }
+}
