523220d67a941088ae19d764b0296d29a6fd0df6
Hunter Busby <10984558@uvu.edu>
2022-10-26 18:17:57 -0600

    6a.2 completed
    Followed the video instructions

---
+++ UFO Defense Force Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+
+    public float speed;
+
+    public float xRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set horizontalInput to receive values from keyboard  
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        // Moves Player left and Right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep Player within bounds.
+        //Left side wall 
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //Right side wall
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
