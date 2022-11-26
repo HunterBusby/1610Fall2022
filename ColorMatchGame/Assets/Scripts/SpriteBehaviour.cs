c3e305c22d86e87fe48bebe4b0ebedfd88ec0ebb
Hunter Busby <10984558@uvu.edu>
2022-11-25 17:33:36 -0700

    7b challenge

--- ColorMatchGame/Assets/Scripts/SpriteBehaviour.cs
+++ ColorMatchGame/Assets/Scripts/SpriteBehaviour.cs
@@ -17,4 +17,9 @@ public class SpriteBehaviour : MonoBehaviour
     {
         rendererObj.color = obj.value;
     }
+
+    public void ChangeRendererColor(ColorIDDataList obj)
+    {
+        rendererObj.color = obj.currentColor.value;
+    }
 }
